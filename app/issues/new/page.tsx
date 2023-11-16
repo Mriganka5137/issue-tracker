7765cbc995a9cbb3a0b637705b565b4f2f8d757e
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-16 12:37:35 +0530

    Add SimpleMdeReact for issue description input and easymde package for markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import React from "react";
+import SimpleMdeReact from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return (
     <div className=" space-y-4 max-w-xl">
       <Input type="text" placeholder="Title" className=" " />
-      <Textarea placeholder="Description" className=" " />
+      <SimpleMdeReact placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
