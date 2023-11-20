432e64bf05ac8bbb000bf4d8f2c7a57ce27df2ef
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-20 10:25:18 +0530

    Fix issue deletion functionality

--- app/issues/[id]/IssueDeleteAction.tsx
+++ app/issues/[id]/IssueDeleteAction.tsx
@@ -13,46 +13,73 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import error from "next/error";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const IssueDeleteAction = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button
-          className="border border-red-400 group hover:bg-red-400"
-          variant="outline"
-        >
-          <DeleteIcon />
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
-          <AlertDialogDescription>
-            Are you sure you want to delete this issue? This action cannot be
-            undone.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel className=" bg-secondary/50">
-            Cancel
-          </AlertDialogCancel>
-          <AlertDialogAction
-            className="bg-red-400 hover:bg-red-600"
-            onClick={async () => {
-              await axios.delete("/api/issues/" + issueId);
-              router.push("/issues");
-              router.refresh();
-            }}
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button
+            className="border border-red-400 group hover:bg-red-400"
+            variant="outline"
           >
-            Continue
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+            <DeleteIcon />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
+            <AlertDialogDescription>
+              Are you sure you want to delete this issue? This action cannot be
+              undone.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className=" bg-secondary/50">
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-red-400 hover:bg-red-600"
+              onClick={deleteIssue}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <AlertDialog open={error}>
+        <AlertDialogContent>
+          <AlertDialogTitle>Error</AlertDialogTitle>
+          <AlertDialogDescription>
+            This issue could not be deleted.
+          </AlertDialogDescription>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              className=" bg-secondary/50"
+              onClick={() => setError(false)}
+            >
+              Cancel
+            </AlertDialogCancel>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
