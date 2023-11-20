8e8f4b0549770fea83d0805786f0bb574ab15dea
Mriganka5137 <gogoimriganka09@gmail.com>
2023-11-20 20:02:29 +0530

    Add React Query for fetching users

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Select,
@@ -9,21 +10,40 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import prisma from "@/prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { User } from "@prisma/client";
+import { Skeleton } from "@/components/ui/skeleton";
 
-const AsigneeSelect = async () => {
-  let users;
-  try {
-    users = await prisma.user.findMany({
-      orderBy: {
-        name: "asc",
-      },
-    });
-  } catch (error) {
-    throw new Error();
-  }
-  if (!users) {
-    return;
-  }
+const AsigneeSelect = () => {
+  // let users;
+  // try {
+  //   users = await prisma.user.findMany({
+  //     orderBy: {
+  //       name: "asc",
+  //     },
+  //   });
+  // } catch (error) {
+  //   throw new Error();
+  // }
+  // if (!users) {
+  //   return;
+  // }
+
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, // 60 sec
+    retry: 3,
+  });
+
+  if (isLoading) return <Skeleton className="w-40 h-10 bg-gray-200" />;
+  if (error) return null;
+
   return (
     <Select>
       <SelectTrigger className="w-[150px] bg-secondary flex-1">
@@ -32,8 +52,12 @@ const AsigneeSelect = async () => {
       <SelectContent className=" bg-secondary">
         <SelectGroup>
           <SelectLabel>Asignee</SelectLabel>
-          {users.map((user) => (
-            <SelectItem key={user.id} value={user.id}>
+          {users?.map((user) => (
+            <SelectItem
+              key={user.id}
+              value={user.id}
+              className="cursor-pointer "
+            >
               {user.name}
             </SelectItem>
           ))}
